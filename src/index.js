24d1b9b3e7e478c21f87b7f0b983cf7c473c5ecc
Chad <chadjcampbell@gmail.com>
2022-09-12 14:56:08 -0400

    setup project sidebar script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import '../src/style.scss';
+
+const projectList = document.querySelector('#projectList')
+const projectForm = document.querySelector('#projectForm')
+const projectInput = document.querySelector('#projectInput')
+
+let projects = [
+    {id: 0, name: 'General'}
+]
+let i = 0
+
+projectForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const projectName = projectInput.value
+    if (projectName === '' || projectName === null) return
+    const project = createProject(projectName)
+    projectInput.value = null
+    projects.push(project)
+    renderProjects()
+})
+
+function createProject(name) {
+    i++
+    return {id: i, name: name, tasks: []}
+}
+
+function renderProjects () {
+    clearElement(projectList)
+    projects.forEach(project => {
+        const newProject = document.createElement('li')
+        newProject.dataset.projectId = project.id
+        newProject.textContent = project.name
+        projectList.append(newProject)
+    })
+
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+renderProjects()
